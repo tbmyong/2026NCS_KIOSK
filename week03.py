5ec5468f00da8896eddb230bba98fa5ed0a129e8
siyoungYim <peepcloud@inha.edu>
2026-03-17 17:19:29 +0900

    week03 average 8

--- week03.py
+++ week03.py
@@ -1,17 +1,11 @@
 import numpy as np
 
 items = [40, 7, 99, -3]
-items_new = []
-for item in items:
-    items_new.append(item + 5)
-print(items_new)
-
-
-items = [40, 7, 99, -3]
-items =list(map(lambda i: i+5, items))
-print(items)
-
-#위와 결과 동일 모든 값에 +5되어 출력된다
-#data1 = np.array([40, 7, 99, -3])
-#print(data1 + 5)
+print(np.sum(items))
+print(np.mean(items))
+print(np.max(items))
+print(np.min(items))
+print(np.median(items))
+print(np.var(items))
+print(np.std(items))
 
