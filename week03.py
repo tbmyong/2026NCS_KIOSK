1b33820e9b2ed8fb4e3d55d3fe3ff1ee5bd86c68
siyoungYim <peepcloud@inha.edu>
2026-03-17 18:48:41 +0900

    week03 dataframe 4

--- week03.py
+++ week03.py
@@ -1,23 +1,12 @@
 import numpy as np
-import pandas as pd #pandas는 labeling이 가능하다
+import seaborn as sns
 
-items = {'a': [100, 80, 90],
-         'b': [95, 75, 85],
-         'c': [10, 80, 90]
-         }
+mpg = sns.load_dataset('mpg')
+print(mpg.head(3))
+print(mpg.tail(3))
+print(mpg.query('mpg>27.0'))
 
-df_items = pd.DataFrame(items, index=[1,2,3])
-print(df_items)
-
-
-items = [
-    [100, 95, 10],
-    [80, 75, 80],
-    [90, 85, 90]
-]
-
-df_items = pd.DataFrame(items, index=[1,2,3], columns=['a','b','c'])
-print(df_items)
-df_items_melt = pd.melt(df_items).rename(columns={'variable':'var','value':'val'}).query('val >= 85')
-print(df_items_melt)
-
+mpg_asc = mpg.sort_values('mpg')
+print(mpg_asc)
+msg_asc_disp_out = mpg_asc.drop(columns=['displacement'])
+print(msg_asc_disp_out)
