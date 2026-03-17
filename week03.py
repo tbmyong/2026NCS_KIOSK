a8f939bc7a36203d8b2ddac17fecc246d57dd04c
siyoungYim <peepcloud@inha.edu>
2026-03-17 17:47:06 +0900

    week03 average 9

--- week03.py
+++ week03.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-items = [40, 7, 99, -3]
-print(np.sum(items))
-print(np.mean(items))
-print(np.max(items))
-print(np.min(items))
-print(np.median(items))
-print(np.var(items))
-print(np.std(items))
+scores = np.array([
+    #국, 영, 수
+    [80, 100, 90], #A 학생
+    [85, 75, 95], #B 학생
+    [89, 85, 87], #C 학생
+])
 
+
+print(f"전체 평균 : {np.mean(scores)}")
+print(f"국 영 수 과목별 평균 : {np.mean(scores, axis=0)}")  #열 기준 연산
+print(f"A, B, C 학생 최고점수: {np.max(scores, axis=1)}") #행 기준 연산
