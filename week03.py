92a4a6324be0d484c07aa5442d57fe8035e60fdf
siyoungYim <peepcloud@inha.edu>
2026-03-17 17:51:44 +0900

    week03 dataframe 1

--- week03.py
+++ week03.py
@@ -1,13 +1,10 @@
 import numpy as np
+import pandas as pd #pandas는 labeling이 가능하다
 
-scores = np.array([
-    #국, 영, 수
-    [80, 100, 90], #A 학생
-    [85, 75, 95], #B 학생
-    [89, 85, 87], #C 학생
-])
+items = {'a': [100, 80, 90],
+         'b': [95, 75, 85],
+         'c': [10, 80, 90]
+         }
 
-
-print(f"전체 평균 : {np.mean(scores)}")
-print(f"국 영 수 과목별 평균 : {np.mean(scores, axis=0)}")  #열 기준 연산
-print(f"A, B, C 학생 최고점수: {np.max(scores, axis=1)}") #행 기준 연산
+df_items = pd.DataFrame(items, index=[1,2,3])
+print(df_items)
