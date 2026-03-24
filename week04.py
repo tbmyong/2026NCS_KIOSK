2cab5e319f3661be681432f07739392007609d57
siyoungYim <peepcloud@inha.edu>
2026-03-24 17:37:48 +0900

    week04 dataframe 3

--- week04.py
+++ week04.py
@@ -1,25 +1,14 @@
-import seaborn as sns
 import pandas as pd
-#유럽 제조 차량중 연비가 30.0 mpg이상인 행 추출하시오
-#단, 자동차 모델명, 연비, 제조국가만 표시하고 연비가 높은 순으로 정렬하시오
 
-mpg = sns.load_dataset('mpg')
-group_mpg = pd.DataFrame(mpg.groupby(by='origin'))
-print(group_mpg)
+def sequaes(n):
+    return n * n
+items = {
+    'a' : [100, 80, 90],
+    'b' : [100, 95, 75],
+    'c' : [100, 60, 90]
+}
 
-print(mpg['origin'].nunique)
-print(mpg.describe())
-#mpg = mpg.drop(columns=['cylinders','displacement','horsepower','weight'
-                                                               , 'model_year'])
-
-#print(mpg)
-#print(mpg.iat[1,8])
-#print(mpg[['origin','mpg','name']])
-#print(mpg[['origin','mpg','name']].query('origin == "europe" and mpg >30.0')) #이렇게 하면 europe에서 key값 missing
-#print(mpg[['origin','mpg','name']]
-#      .query('origin == "europe" and mpg >30.0')
-#      .sort_values('mpg', ascending=False))
-#print(mpg.iloc[1:4,[0,2]])
-#print(mpg)
-#print(mpg.info)
-
+df_itmes = pd.DataFrame(items, index=[1,2,3])
+#print(df_items)
+print(df_itmes.apply(sequaes)) #애는 squares랑 같이 쓰거나 아님 람다만 혼자 씀
+print(df_itmes.apply(lambda n : n * n))
