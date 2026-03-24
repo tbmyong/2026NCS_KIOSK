56ebc58ccf294e1fb2957b66f7bacef511cf2e0a
siyoungYim <peepcloud@inha.edu>
2026-03-24 17:59:16 +0900

    week04 dataframe 4

--- week04.py
+++ week04.py
@@ -1,14 +1,9 @@
 import pandas as pd
+import seaborn as sns
 
-def sequaes(n):
-    return n * n
-items = {
-    'a' : [100, 80, 90],
-    'b' : [100, 95, 75],
-    'c' : [100, 60, 90]
-}
-
-df_itmes = pd.DataFrame(items, index=[1,2,3])
-#print(df_items)
-print(df_itmes.apply(sequaes)) #애는 squares랑 같이 쓰거나 아님 람다만 혼자 씀
-print(df_itmes.apply(lambda n : n * n))
+mpg = sns.load_dataset('mpg')
+print(mpg.info())
+#mpg.dropna(subset=['horsepower'], inplace=True) #원본 저장
+#print(mpg.info())
+mpg_clean = mpg.dropna(subset=['horsepoewr']) #사본 생성
+print(mpg_clean)
