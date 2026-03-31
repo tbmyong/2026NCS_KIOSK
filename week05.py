59fe030a174ae9f174fe358c61a4d7741ad8a5d5
siyoungYim <peepcloud@inha.edu>
2026-03-31 16:56:52 +0900

    week05 ocp2

--- week05.py
+++ week05.py
@@ -1,20 +1,34 @@
 import time
 
+def time_measure_decorator(f):
+    def wrapper(*args):
+        s = time.time()
+        r = f(*args)
+        e = time.time()
+        print(e-s)
+       return r
+    return wrapper
+
+
+@time_measure_decorator
+
+
 def one_to_n_loop(n):
-    s = time.time()
+
     result = 0
     for i in range(1,n+1):
         result = result + i
-    e = time.time()
-    print(e-s)
+
     return result
 
+@time_measure_decorator
+
 def ont_to_n_match(n):
-    s = time.time()
+
     r = n * (n + 1)//2
-    e = time.time()
-    print(e-s)
+
     return r
+
 number = int(input("Input number : "))
 print(ont_to_n_match(number))
 print(one_to_n_loop(number))
